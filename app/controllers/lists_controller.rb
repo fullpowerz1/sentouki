be5b32f3683372e4a0ad2aa57b17fd87e06d4770
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-17 09:27:28 +0000

    igu

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,8 +24,7 @@ class ListsController < ApplicationController
     # 4. トップ画面へリダイレクト
       redirect_to list_path(@list.id)
     else
-      @lists = List.all
-      render :index
+      render :new
     end
   end
 
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,7 @@ class List < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+  # あとでコメントアウトする。
   validates :image, presence: true
+
 end
